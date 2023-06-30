a96454d4235c0bd3d5a3877f4640cda9f89b2a1f
Vladislav <wilprotsenko@mail.ru>
2023-06-30 20:08:22 +0700

    Дело сделано

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/home/home'
 import { HeaderPage } from './pages/header' 
 import Catalog from './pages/catalog'
-import Collection from './pages/collection'
 import Help from './pages/help'
 import Magazini from './pages/magazini'
 import Contacti from './pages/contacti'
@@ -18,7 +17,6 @@ function App() {
         <Routes>
           <Route path={'/'} element={<HomePage />} />
           <Route path={'/catalog'} element={<Catalog />} />
-          <Route path={'/collection'} element={<Collection />} />
           <Route path={'/help'} element={<Help />} />
           <Route path={'/contacti'} element={<Contacti />} />
           <Route path={'/magazini'} element={<Magazini />} />
